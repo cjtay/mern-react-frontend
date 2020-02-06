8b9b43bebc8efff9de0ba4147c57e7ce5ec7a747
Tay CJ <cjtay@outlook.sg>
2020-02-06 14:15:57 +0800

    fixed animation for sidedrawer

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -10,35 +10,36 @@ import './MainNavigation.css';
 const MainNavigation = props => {
     const [drawerIsOpen, setDrawerIsOpen] = useState(false);
 
-    const openDrawer = () => {
+    const openDrawerHandler = () => {
         setDrawerIsOpen(true);
     };
 
-    const closeDrawer = () => {
+    const closeDrawerHandler = () => {
         setDrawerIsOpen(false);
     };
 
     return (
         <>
-            {drawerIsOpen && <Backdrop onClick={closeDrawer} />}
-            {drawerIsOpen && (
+            {drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
+            <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
+                >
+                <nav className='main-navigation__drawer-nav'>
+                    <NavLinks />
+                </nav>
+            </SideDrawer>
+
+            {/* {drawerIsOpen && (
                 <SideDrawer>
                     <nav className='main-navigation__drawer-nav'>
                         <NavLinks />
                     </nav>
                 </SideDrawer>
-            )}
-
-            {/* <SideDrawer show={drawerIsOpen}>
-                <nav className='main-navigation__drawer-nav'>
-                    <NavLinks />
-                </nav>
-            </SideDrawer> */}
+            )} */}
 
             <MainHeader>
                 <button
                     className='main-navigation__menu-btn'
-                    onClick={openDrawer}
+                    onClick={openDrawerHandler}
                 >
                     <span />
                     <span />
