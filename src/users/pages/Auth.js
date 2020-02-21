ac2e95cc338047e909b6a76192120fd14f114955
Tay CJ <cjtay@outlook.sg>
2020-02-22 00:26:57 +0800

    connected signup

--- src/users/pages/Auth.js
+++ src/users/pages/Auth.js
@@ -15,7 +15,7 @@ import {
 import './Auth.css';
 
 const Auth = () => {
-    const { login } = useContext(AuthContext);
+    const auth = useContext(AuthContext);
     const [isLoginMode, setIsLoginMode] = useState(true);
     const [formState, inputHandler, setFormData] = useForm(
         {
@@ -31,10 +31,33 @@ const Auth = () => {
         false
     );
 
-    const authSubmitHandler = event => {
+    const authSubmitHandler = async event => {
         event.preventDefault();
-        console.log('Authenticating', formState.inputs);
-        login();
+
+        if (isLoginMode) {
+        } else {
+            try {
+                const response = await fetch(
+                    'http://localhost:5000/api/users/signup',
+                    {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({
+                            name: formState.inputs.name.value,
+                            email: formState.inputs.email.value,
+                            password: formState.inputs.password.value
+                        })
+                    }
+                );
+                const responseData = await response.json();
+                console.log(responseData);
+            } catch (err) {
+                console.log(err);
+            }
+        }
+        auth.login();
     };
 
     const switchModeHandler = () => {
